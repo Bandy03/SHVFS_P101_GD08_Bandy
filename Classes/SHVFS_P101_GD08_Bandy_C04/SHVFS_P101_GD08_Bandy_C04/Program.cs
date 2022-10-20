f426d0aa9e1c7cfca5b0571fde872b214fbf1e5b
Bandy03 <115708257+Bandy03@users.noreply.github.com>
2022-10-20 22:24:28 +0800

    111

---
+++ Classes/SHVFS_P101_GD08_Bandy_C04/SHVFS_P101_GD08_Bandy_C04/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace SHVFS_P101_GD08_C4_Bandy
+{
+    public class program
+    {
+        public static void Main(string[] arge)
+        {
+            //var arithmeticOperatorMultiply = 5;
+            //Console.WriteLine(arithmeticOperatorMultiply);
+            //Console.ReadLine()
+            //Console.WriteLine("This is BEDORE our FOR LOOP!");
+
+            //for (var i = 0; i < 10; i++)
+            //{ 
+            //    Console.WriteLine("This is THE FIRST LINE in our FOR LOOP!");
+            //    Console.WriteLine($"The value of i is:{i}");
+            //}
+
+            //Console.WriteLine("This is AFTER our FOR LOOP!");
+            //Console.ReadLine();
+
+            //var input = "";
+
+            //while (input ! = "Quit")
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine($"Enter \"Quit\" to win the game!");
+            //    input= Console.ReadLine();
+            //}
+            //Console.WriteLine("CONGRATS! YOU WON!");
+            //Console.ReadLine();
+
+            //int i;
+            //Console.WriteLine("Enter 1 for options\n" +
+            //    "2 to quit\n" +
+            //    "3 to strat game");
+
+            //do
+            //{
+            //    Console.WriteLine("Please make a selection! HAHA!");
+            //    i = Convert.ToInt32(Console.ReadLine());
+            //}   while(i < 1|| i>3);
+
+            //Console.WriteLine("Excellent choice");
+            //Console.WriteLine();
+            //for (var i = 0; i < 7; i++)
+            //{
+            //    for (var j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine($"i is: {i}, and j is: {j}");
+            //    }
+            //}
+
+            //Console.ReadLine();
+
+            //var scores = new int[5];
+            //scores[0] = 7;
+            //scores[1] = 24;
+            //scores[2] = 2;
+            //scores[3] = 9;
+            //scores[4] = 1;
+            //scores[5] = 886;
+            //scores[6] = 99;
+
+            //for (var i = 0; i < scores.Length; i++)
+            //{
+            //    Console.WriteLine($"The length of scores is: {scores.Length} | The index is;{1} | The value is: scores[i]");
+            //}
+
+            //Console.WriteLine();
+
+            var score = new int[5] { 55, 3, 24, 29, 33 };
+            //var score = new int[] { 55, 3, 24, 29, 33 };
+            //var score = new [] { 55, 3, 24, 29, 33 };
+
+
+
+            for (var i = 0; i < score.Length; i++) ;
+            {
+                Console.WriteLine
+            }
+        }
+    }
+}
