a6a8c0d747ffd9a409b8e7e1428f64b7738d0f3b
Bandy03 <115708257+Bandy03@users.noreply.github.com>
2022-11-11 17:13:54 +0800

    1

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Bandy/SHVFS_P101_GD08_Q2022-11-10_Bandy/SHVFS_P101_GD08_Q2022-11-10_Bandy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+namespace SHVFS_P101_C6_GD_Bandy
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name");
+            string B =Console.ReadLine();
+
+            int C = 0;
+
+            string[] food = new string[] {"rice","noodle" };
+            Random random = new Random();
+            int A = random.Next(food.Length);
+            string S = "";
+            while (S!= food[A])
+            {
+                Console.Clear();
+                C++;
+                Console.WriteLine($"{B} Guess a word");
+                S = Console.ReadLine();
+            }
+            Console.WriteLine($"You guess {C} times, Right");
+        }
+    }
+}
